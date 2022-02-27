f73df17e1474399a016d96eceba22f9c7da2ff2e
manish6007 <manishshrivastava26@gmail.com>
2022-02-27 12:18:03 +0530

    Pipeline

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from flask import Flask, jsonify, abort, make_response
 APP = Flask(__name__)
 
 # Load the data
-MASCOTS = json.load(open('data.json', 'r'))
+with open('data.json', 'rb') as f:
+    MASCOTS = json.load(f)
 
 
 @APP.route('/', methods=['GET'])
